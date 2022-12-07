cfefbe728403e60d87232bfe8e7fdd8743886322
caratofficial <lilyrealsteel@gmail.com>
2022-12-07 16:15:05 +0700

    Init

--- 03-table/product.js
+++ 03-table/product.js
@@ -23,25 +23,26 @@ var products = [
 ]
 
 function loadData() {
-    var elem = document.getElementById("myName")
-    elem.innerHTML = "Thanyaluck Arunrattanakul"
 
-    var productList = document.getElementById("productList")
-
+    let productList = document.getElementById("productList")
+    let gross = 0
     for (let p in products) {
-       var row = document.createElement("tr")
-       var data = document.createElement("td")
-       data.innerHTML = products[p].name
+       let row = document.createElement("tr")
+       let productName = document.createElement("td")
+       productName.innerHTML = products[p].name
 
        let quantity = document.createElement("td")
        quantity.innerHTML = products[p].quantity
+       quantity.classList.add("text-right")
 
        let ppu = document.createElement("td")
-        ppu.innerHTML = products[p].ppu
+       ppu.innerHTML = products[p].ppu
+       ppu.classList.add("text-right")
 
        let total = document.createElement("td")
        total.innerHTML = products[p].ppu * products[p].quantity 
-
+       total.classList.add("text-right")
+       gross += products[p].ppu * products[p].quantity
 
        row.appendChild(productName)
        row.appendChild(quantity)
@@ -49,5 +50,13 @@ function loadData() {
        row.appendChild(total)
        productList.appendChild(row)
     }
+    let grossElem = document.getElementById("gross")
+    grossElem.innerHTML = gross
+
+    let vat = gross * 0.07
+    let net = gross + vat
+    document.getElementById("vat").innerHTML = vat.toFixed(2)
+    document.getElementById("net").innerHTML = net.toFixed(2)
+
 
 }
