538218d173c917e3f941ba7adeb3f165d33e308f
caratofficial <lilyrealsteel@gmail.com>
2022-12-07 15:30:47 +0700

    Init

---
+++ 03-table/product.js
@@ -0,0 +1,28 @@
+m// define data in JSON array
+var products = [
+    {
+        name: "Samsung TV 50",
+        quantity: 1,
+        ppu: 15000
+    },
+    {
+        name: "Xiaomi Fan",
+        quantity: 2,
+        ppu: 500
+    },
+    {
+        name: "iPhone 14 Pro Max Ultra",
+        quantity: 1,
+        ppu: 50000
+    },
+    {
+        name: "Logitec Gaming Mouse",
+        quantity: 1,
+        ppu: 4000
+    }
+]
+
+function loadData() {
+    var elem = document.getElementById("myName")
+    elem.innerHTML ="Thanyaluck Arunrattanakul"
+}
