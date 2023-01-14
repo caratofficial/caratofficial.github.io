a9fc322c6795f5b47dac0bc1101f2f73acded74f
caratofficial <lilyrealsteel@gmail.com>
2023-01-14 17:06:31 +0700

    init

--- Midterm/Quotation.js
+++ Midterm/Quotation.js
@@ -1,4 +1,4 @@
-var customers = []
+var products = []
 
 $(document).ready(function () {
     console.log("ready!");
@@ -10,52 +10,52 @@ $(document).ready(function () {
         console.log("DONE",data)
         for(let d in data){
             // save the data record into our local variable
-            customers.push(data[d])
+            products.push(data[d])
             let dataStr = `<tr>
-                <td> <img src='delete.png' width='20em' onclick='deleteCustomer("${d}")'/> ${data[d].name} </td>
-                <td>${data[d].email}</td>
-                <td>${data[d].phone}</td>
+    //            <td> <img src='delete.png' width='20em' onclick='deleteCustomer("${d}")'/> ${data[d].Item} </td>
+                <td>${data[d].Priceperunit}</td>
+                <td>${data[d].Quantity}</td>
             </tr>`
             $("#data-table tr:last").after(dataStr)
         }
 
-        console.log(customers)
+        console.log(products)
         loadData();
     });
 });
 
-function addCustomer() {
-    let customerObj = {
-        name: $('#exampleFormControlInput1').val(),
-        email: $('#exampleFormControlInput2').val(),
-        phone: $('#exampleFormControlInput3').val()
+function addItem() {
+    let ItemObj = {
+        Item: $('#exampleFormControlInput1').val(),
+        Priceperunit: $('#exampleFormControlInput2').val(),
+        Quantity: $('#exampleFormControlInput3').val()
     }
     
     $('#data-table').html("")
 
 
-    customers.push(customerObj)
-    console.log(customers)
+    products.push(ItemObj)
+    console.log(products)
 
-    let index = customers.length - 1
+    let index = products.length - 1
     loadData()
 
 }
 
     function deleteCustomer(index) {
         console.log("DELETE",index)
-        delete customers[index]  // delete the element from array
+        delete products[index]  // delete the element from array
         $('#data-table').html("")
         loadData()
     }
 
     function loadData() {
         let allRows = ""
-        for (let p in customers) {
-            let cellName = `<td><img class='icon' width='20em' src='delete.png' onclick='deleteCustomer("${p}")'> ` + customers[p].name + "</td>"
-            let cellEmail = '<td>' + customers[p].email + "</td>"
-            let cellPhone = '<td class="text-right">' + customers[p].phone + "</td>"
-            let row = `<tr>${cellName}${cellEmail}${cellPhone}</tr>`
+        for (let p in products) {
+            let cellItem = `<td><img class='icon' width='20em' src='delete.png' onclick='deleteCustomer("${p}")'> ` + products[p].Item + "</td>"
+            let cellPriceperunit = '<td>' + products[p].Priceperunit + "</td>"
+            let cellQuantity = '<td class="text-right">' + products[p].Quantity + "</td>"
+            let row = `<tr>${cellItem}${cellPriceperunit}${cellQuantity}</tr>`
             allRows += row
         }
         $('#data-table').html(allRows)
